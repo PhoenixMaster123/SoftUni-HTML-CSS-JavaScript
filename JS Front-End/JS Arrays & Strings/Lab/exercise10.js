ffa3cdad0aea1b57d09ab2da0d3ef49b3247904f
Kristian Popov <kristianpopov03@yahoo.com>
2025-02-17 21:57:19 +0100

    JS Syntax Fundamentals

---
+++ JS Front-End/JS Arrays & Strings/Lab/exercise10.js
@@ -0,0 +1,6 @@
+function printNumbers(num1, num2) {
+    for(let i = num1; i >= num2; i--) {
+        console.log(i);
+    }
+}
+printNumbers(6, 2);
---
+++ JS Front-End/JS Arrays & Strings/Lab/exercise2.js
@@ -0,0 +1,6 @@
+function print(name, age, grade) {
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`);
+}
+
+// Example:
+print('John', 15, 5.54678) // -> Name: John, Age: 15, Grade: 5.55
